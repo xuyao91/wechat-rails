986a112749f861c687fb9a2e6f97e9a51c63eb5b
xuyao <xuyao@cloudoc.cn>
2015-07-27 15:19:36 +0800

    add access_token expire process

--- lib/wechat/access_token.rb
+++ lib/wechat/access_token.rb
@@ -20,9 +20,17 @@ module Wechat
 
     def refresh
       data = client.get("token", params:{grant_type: "client_credential", appid: appid, secret: secret})
-      File.open(token_file, 'w'){|f| f.write(data.to_s)} if valid_token(data)
+      puts "---------refresh token --------#{data}----------------"
+      File.open(token_file, 'w'){|f| f.write(data.merge(expires_at: expired_at).to_json)} if valid_token(data)
       return @token_data = data
     end
+
+    def read_token
+      token_data = JSON.parse(file.read(token_file))
+      return self.refresh if expire? token_data
+      puts "---------no need refresh token --------#{token_data}----------------"
+      return token_data  
+    end  
 
     private 
     def valid_token token_data
@@ -30,6 +38,14 @@ module Wechat
       raise "Response didn't have access_token" if  access_token.blank?
       return access_token
     end
+
+    def expired_at 
+      (Time.now.to_i + 7200) - 10
+    end  
+
+    def expire? token_data
+      Time.now.to_i >= token_data["expired_at"].to_i
+    end  
 
   end
 end
